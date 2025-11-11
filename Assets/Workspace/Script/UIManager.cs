de5fb28627657df43f9508d1722d70148f38d323
Khiem07 <131628581+Khiem07@users.noreply.github.com>
2025-11-11 17:16:16 +0700

    Introduction to Unity UI – Part 2_Completed

--- Assets/Workspace/Script/UIManager.cs
+++ Assets/Workspace/Script/UIManager.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,4 +28,19 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene("RocketMouse");
     }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
+
 }
